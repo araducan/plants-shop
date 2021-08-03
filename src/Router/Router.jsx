7faacc8a0ac1ec8190b06ebf61042dc7e07bfc4a
Andreea R <araducan0805@gmail.com>
2021-08-03 15:25:45 +0300

    added mixins + contact route

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Contact from "../components/Contact/Contact";
 import Header from "../components/Header/Header";
 import Home from "../components/Home/Home";
 
 const Router = () => {
-  return (
-    <BrowserRouter>
-      <Header></Header>
-      <Switch>
-        <Route path="/" component={Home} />
-      </Switch>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Header></Header>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/contact" component={Contact} />
+			</Switch>
+		</BrowserRouter>
+	);
 };
 
 export default Router;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,40 +6,40 @@ import { faShopify } from "@fortawesome/free-brands-svg-icons";
 import { faSistrix } from "@fortawesome/free-brands-svg-icons";
 
 const Header = () => {
-  return (
-    <div className={style.container}>
-      <div className={`${style.subcontainer} ${style.logo}`}>plant.</div>{" "}
-      <div className={style.subcontainer}>
-        <Link className={style.link} to="/">
-          Home
-        </Link>
-        <Link className={style.link} to="/">
-          Shop
-        </Link>
-        <Link className={style.link} to="/">
-          About
-        </Link>
-        <Link className={style.link} to="/">
-          Contact
-        </Link>
-      </div>
-      <div className={style.subcontainer}>
-        <div className={style.search}>
-          <FontAwesomeIcon
-            className={style.icon}
-            icon={faSistrix}
-          ></FontAwesomeIcon>
-          <input className={style.input} type="text"></input>
-        </div>
+	return (
+		<div className={style.container}>
+			<div className={`${style.subcontainer} ${style.logo}`}>plant.</div>{" "}
+			<div className={style.subcontainer}>
+				<Link className={style.link} to="/">
+					Home
+				</Link>
+				<Link className={style.link} to="/">
+					Shop
+				</Link>
+				<Link className={style.link} to="/">
+					About
+				</Link>
+				<Link className={style.link} to="/contact">
+					Contact
+				</Link>
+			</div>
+			<div className={style.subcontainer}>
+				<div className={style.search}>
+					<FontAwesomeIcon
+						className={style.icon}
+						icon={faSistrix}
+					></FontAwesomeIcon>
+					<input className={style.input} type="text"></input>
+				</div>
 
-        <FontAwesomeIcon
-          className={style.icon}
-          id={style.cart}
-          icon={faShopify}
-        ></FontAwesomeIcon>
-      </div>
-    </div>
-  );
+				<FontAwesomeIcon
+					className={style.icon}
+					id={style.cart}
+					icon={faShopify}
+				></FontAwesomeIcon>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
