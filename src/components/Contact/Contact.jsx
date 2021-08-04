4a4bb51b90364952ac0d033b952e677d950d81b7
Andreea R <araducan0805@gmail.com>
2021-08-04 07:59:24 +0300

    Contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import style from "./Contact.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faPhone,
+	faEnvelope,
+	faMapMarkedAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
 	return (
@@ -14,6 +20,40 @@ const Contact = () => {
 					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
 					pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
 					culpa qui officia deserunt mollit anim id est laborum.
+				</div>
+				<div className={style.footer}>
+					<div className={style.bottomContainer}>
+						<FontAwesomeIcon
+							className={style.icon}
+							icon={faPhone}
+						></FontAwesomeIcon>
+						<div>
+							<div className={style.subtitle}>Phone number</div>
+							(+40) 761815578
+						</div>
+					</div>
+
+					<div className={style.bottomContainer}>
+						<FontAwesomeIcon
+							className={style.icon}
+							icon={faEnvelope}
+						></FontAwesomeIcon>
+						<div>
+							<div className={style.subtitle}>E-mail</div>
+							plants@gmail.com
+						</div>
+					</div>
+
+					<div className={style.bottomContainer}>
+						<FontAwesomeIcon
+							className={style.icon}
+							icon={faMapMarkedAlt}
+						></FontAwesomeIcon>
+						<div>
+							<div className={style.subtitle}>Address</div>
+							3425 Stone Street, Apt. 2A, Jacksonville, FL 39404
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
